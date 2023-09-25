65638bf8f29a0cae4d76d5c3820fa06987310c4f
Sergio Rodriguez <ingensiscomescom@outlook.com>
2023-09-25 17:39:27 -0600

    Versión 1.0 de escalas musicales(Responsive)

--- Web/public/js/escalas.js
+++ Web/public/js/escalas.js
@@ -2,12 +2,17 @@
 const NOTAS = ['Do','Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
 
 //HTML
-const notaSelected = document.getElementById("nota-musical")
-const escalaSelected = document.getElementById("escala-musical")
-const muestraEscala = document.querySelector(".muestra-escala")
+var notaSelected = document.getElementById("nota-musical")
+var escalaSelected = document.getElementById("escala-musical")
+var muestraEscala = document.querySelector(".muestra-escala")
+var muestraDescription = document.querySelector(".contenedor-descripcion")
 
 //VARIABLES GLOBALES
 var arregloEscala = ['Do','Re', 'Mi', 'Fa', 'Sol', 'La', 'Si', 'Do']
+var arregloAux = NOTAS.slice()
+
+//ERRORES
+const errorEtiqueta = "No existe sección.\nCreando ";
 
 /*
 
@@ -43,39 +48,38 @@ var arregloEscala = ['Do','Re', 'Mi', 'Fa', 'Sol', 'La', 'Si', 'Do']
 */
 
 //Validamos el arreglo
-function valArray(val_nota, aux){
+function valArray(val_nota){
     //Convertimos la primera letra a mayúscula
     var nota = val_nota[0].toUpperCase() + val_nota.substring(1);
 
     //Buscando la nota
     var position = NOTAS.indexOf(nota)
     
-    for (let i = 0; i < aux.length; i++) {
+    for (let i = 0; i < arregloAux.length; i++) {
         if (position == 0) {
-            //i = aux.length
-            aux = NOTAS.slice()
+            arregloAux = NOTAS.slice()
             return;
         } else {
-            if (position == aux.length) {
+            if (position == arregloAux.length) {
                 position = 0
             }
             
-            aux[i] = NOTAS[position]
+            arregloAux[i] = NOTAS[position]
             position++
         }
     }
 }
 
-function eliminarMostrar(eliminarHijos){
+//Eliminamos una sección
+function eliminarSection(eliminarHijos){
     if (eliminarHijos !== null) {
         while (eliminarHijos.hasChildNodes()){
             eliminarHijos.removeChild(eliminarHijos.lastChild);
         }
-    } else {
-        alert("La etiqueta no existe")
     }
 }
 
+//Agregamos una nota en una sección especifica
 function agregarNota(section, texto){
     const item = document.createElement("p");
     item.className = "muestra-escala"
@@ -86,7 +90,57 @@ function agregarNota(section, texto){
     section.appendChild(item);
 }
 
-function escalaMayor(arreglo, aux, muestra){
+//Agregamos la descripción de las estructuras
+function descripcion(escala, estructura){
+
+    const titulo = document.createElement("h3");
+    titulo.className = "titulo-descripcion";
+
+    let text = document.createTextNode("Estructura de la escala "+ escala);
+    
+    titulo.appendChild(text);
+
+    //Creamos la sección para colocar el parrafo de la estructura
+    const parrafo = document.createElement("p");
+    parrafo.className = "descripcion";     //Le damos una clase al parrafo
+
+    text = document.createTextNode(estructura); //Especificamos el texto
+
+    parrafo.appendChild(text) //Agregamos el texto a "p"
+
+    try {
+        if (!muestraDescription) throw errorEtiqueta+"descripción";
+    } catch (error) {
+        console.log(error)
+
+        muestraDescription = crearSection(muestraDescription, "contenedor-descripcion flex")
+    }finally{
+        muestraDescription?.appendChild(titulo) //Agregamos el titulo a la sección 
+        muestraDescription?.appendChild(parrafo) //Agregamos el parrafo a la sección 
+    }
+}
+
+//Crea secciones dentro del main
+function crearSection(section, clases){
+    //Si no hay ningún div...
+    if(section == null){
+        //Creamos el elemento/objeto que deseamos
+        let creaSection = document.createElement("section");
+        
+        //Agregamos las clases
+        creaSection.className = clases;
+
+        var main = document.querySelector("main")
+
+        main?.appendChild(creaSection);
+
+        section = creaSection;
+    }
+
+    return section;
+}
+
+function escalaMayor(arreglo, section){
 
     /*
     Estructura de la escala mayor
@@ -96,18 +150,18 @@ function escalaMayor(arreglo, aux, muestra){
 
     for (let i = 0; i < arreglo.length-1; i++) {
         if (i >= 3) {
-            arreglo[i] = aux[i*2-1]
+            arreglo[i] = arregloAux[i*2-1]
         }else{
-            arreglo[i] = aux[i*2]
+            arreglo[i] = arregloAux[i*2]
         }
 
-        agregarNota(muestra, arreglo[i])
+        agregarNota(section, arreglo[i])
     }
-    arreglo[7] = aux[0]
-    agregarNota(muestra, arreglo[7])
+    arreglo[7] = arregloAux[0]
+    agregarNota(section, arreglo[7])
 }
 
-function escalaMenorNat(arreglo, aux, muestra){
+function escalaMenorNat(arreglo, section){
 
     /*
     Estructura de la escala menor natural
@@ -117,21 +171,21 @@ function escalaMenorNat(arreglo, aux, muestra){
 
     for (let i = 0, potencia = 0; i < arreglo.length-1; i++) {
         if (i < 2) {
-            arreglo[i] = aux[i*2]
+            arreglo[i] = arregloAux[i*2]
         }else{
             if (i >= 5) {
                 potencia=1
             }
-            arreglo[i] = aux[i*2-Math.pow(2,potencia)]
+            arreglo[i] = arregloAux[i*2-Math.pow(2,potencia)]
         }
     
-        agregarNota(muestra, arreglo[i])
+        agregarNota(section, arreglo[i])
     }
-    arreglo[7] = aux[0]
-    agregarNota(muestra, arreglo[7])
+    arreglo[7] = arregloAux[0]
+    agregarNota(section, arreglo[7])
 }
 
-function escalaMenorHar(arreglo, aux, muestra){
+function escalaMenorHar(arreglo, section){
 
     /*
     Estructura de la escala menor harmonica
@@ -139,24 +193,26 @@ function escalaMenorHar(arreglo, aux, muestra){
     console.log("Nota -> Tono -> Semi -> Tono -> Tono -> Semi -> Tono y 1/2 -> Semi");
     */
 
+    // @ts-ignore
+    // @ts-ignore
     for (let i = 0, potencia = 0; i < arreglo.length-1; i++) {
         if (i < 2) {
-            arreglo[i] = aux[2*i]
+            arreglo[i] = arregloAux[2*i]
         }else{
             if (i == 5) {
-                arreglo[i] = aux[2*(i-1)]
+                arreglo[i] = arregloAux[2*(i-1)]
             }else{
-                arreglo[i] = aux[2*i-1]
+                arreglo[i] = arregloAux[2*i-1]
             }
         }
-        agregarNota(muestra, arreglo[i])
+        agregarNota(section, arreglo[i])
     }
     
-    arreglo[7] = aux[0]
-    agregarNota(muestra, arreglo[7])
+    arreglo[7] = arregloAux[0]
+    agregarNota(section, arreglo[7])
 }
 
-function escalaMenorMelAsc(arreglo, aux, muestra){
+function escalaMenorMelAsc(arreglo, section){
 
     /*
     Estructura de la escala menor melódica ascendente
@@ -164,20 +220,22 @@ function escalaMenorMelAsc(arreglo, aux, muestra){
     console.log("Nota -> Tono -> Semi -> Tono -> Tono -> Tono -> Tono -> Semi");
     */
 
+    // @ts-ignore
+    // @ts-ignore
     for (let i = 0, potencia = 0; i < arreglo.length-1; i++) {
         if (i < 2) {
-            arreglo[i] = aux[2*i]
+            arreglo[i] = arregloAux[2*i]
         }else{
-            arreglo[i] = aux[2*i-1]
+            arreglo[i] = arregloAux[2*i-1]
         }
-        agregarNota(muestra, arreglo[i])
+        agregarNota(section, arreglo[i])
     }
     
-    arreglo[7] = aux[0]
-    agregarNota(muestra, arreglo[7])
+    arreglo[7] = arregloAux[0]
+    agregarNota(section, arreglo[7])
 }
 
-function escalaMenorMelDes(arreglo, aux, muestra){
+function escalaMenorMelDes(arreglo, section){
 
     /*
     Estructura de la escala menor melódica descendente
@@ -185,53 +243,72 @@ function escalaMenorMelDes(arreglo, aux, muestra){
     console.log("Nota -> Tono -> Tono -> Semi -> Tono -> Tono -> Semi -> Tono");
     */
 
+    // @ts-ignore
+    // @ts-ignore
     for (let i = 0, potencia = 0; i < arreglo.length-1; i++) {
         if (i < 3) {
-            arreglo[i] = aux[2*i]
+            arreglo[i] = arregloAux[2*i]
         }else{
             if (i == 6) {
-                arreglo[i] = aux[2*(i-1)]
+                arreglo[i] = arregloAux[2*(i-1)]
             } else {
-                arreglo[i] = aux[2*i-1]
+                arreglo[i] = arregloAux[2*i-1]
             }
         }
-        agregarNota(muestra, arreglo[i])
+        agregarNota(section, arreglo[i])
     }
 
-    arreglo[7] = aux[0]
-    agregarNota(muestra, arreglo[7])
+    arreglo[7] = arregloAux[0]
+    agregarNota(section, arreglo[7])
 }
 
 function desplegarEscala(){
 
     // @ts-ignore
     var escala = notaSelected.options[notaSelected.selectedIndex].value
+    
     // @ts-ignore
     var tipoEscala = escalaSelected.options[escalaSelected.selectedIndex].value
-    
-    var arregloAux = NOTAS.slice()
+
+    //Es el separador que se mostrara en la descripción(Código unicode)
+    const separador = "\u{2192}"
 
     //Validar el arreglo
-    valArray(escala, arregloAux)
+    valArray(escala)
+    
+    //Validar sección donde se muestran las escalas
+    try {
+        if (!muestraEscala) throw errorEtiqueta+"Muestra escala";
+    } catch (error) {
+        console.log(error)
+
+        muestraEscala = crearSection(muestraEscala, "muestra-escala tarjeta flex")
+    }
     
     //Eliminar los hijos del HTML
-    eliminarMostrar(muestraEscala)
+    eliminarSection(muestraEscala)
+    eliminarSection(muestraDescription)
 
     switch (tipoEscala) {
         case "Mayor":
-            escalaMayor(arregloEscala, arregloAux, muestraEscala)
+            escalaMayor(arregloEscala, muestraEscala)
+            descripcion(tipoEscala, "Nota "+separador+ " Tono "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono "+separador+ " Tono "+separador+ " Tono "+separador+ " Semi")
             break;
-        case "MenorN":
-            escalaMenorNat(arregloEscala, arregloAux, muestraEscala)
+        case "Menor Natural":
+            escalaMenorNat(arregloEscala, muestraEscala)
+            descripcion(tipoEscala, "Nota "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono "+separador+ " Tono")
             break;
-        case "MenorH":
-            escalaMenorHar(arregloEscala, arregloAux, muestraEscala)
+        case "Menor Harmonica":
+            escalaMenorHar(arregloEscala, muestraEscala)
+            descripcion(tipoEscala, "Nota "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono y 1/2 "+separador+ " Semi")
             break;
-        case "MenorMA":
-            escalaMenorMelAsc(arregloEscala, arregloAux, muestraEscala)
+        case "Menor Melódica Ascendente":
+            escalaMenorMelAsc(arregloEscala, muestraEscala)
+            descripcion(tipoEscala, "Nota "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono "+separador+ " Tono "+separador+ " Tono "+separador+ " Tono "+separador+ " Semi")
             break;
-        case "MenorMD":
-            escalaMenorMelDes(arregloEscala, arregloAux, muestraEscala)
+        case "Menor Melódica Descendente":
+            escalaMenorMelDes(arregloEscala, muestraEscala)
+            descripcion(tipoEscala, "Nota "+separador+ " Tono "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono "+separador+ " Tono "+separador+ " Semi "+separador+ " Tono")
             break;
         default:
             alert("Error al seleccionar la opción")
